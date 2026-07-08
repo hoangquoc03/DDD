d8ddcfcd4b9b848b3d9d99ac814986a09f3487c9
lequoc <lequoc150305@quoc150305gmail.com>
2026-07-08 21:10:08 +0700

    Giải pháp sống sót khi giao tiếp Inter-service

---
+++ src/main/java/org/example/ddd_microservice/dto/ProductDTO.java
@@ -0,0 +1,49 @@
+package org.example.ddd_microservice.dto;
+
+public class ProductDTO {
+
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private Integer stockQuantity;
+
+    public ProductDTO(){}
+
+    // getter setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
--- src/main/java/org/example/ddd_microservice/service/OrderService.java
+++ src/main/java/org/example/ddd_microservice/service/OrderService.java
@@ -5,6 +5,7 @@ import org.example.ddd_microservice.entity.Order;
 import org.example.ddd_microservice.exception.*;
 import org.example.ddd_microservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 
@@ -13,8 +14,11 @@ public class OrderService {
 
     private final OrderRepository repository;
 
-    public OrderService(OrderRepository repository) {
+    private final RestTemplate restTemplate;
+
+    public OrderService(OrderRepository repository, RestTemplate restTemplate) {
         this.repository = repository;
+        this.restTemplate = restTemplate;
     }
 
     public OrderResponseDTO create(OrderRequestDTO dto) {
@@ -69,5 +73,34 @@ public class OrderService {
                 order.getTotalAmount()
         );
     }
+    public ProductDTO getProduct(Long id){
+
+        String url =
+                "http://localhost:8082/api/v1/products/" + id;
+
+        try{
+
+            return restTemplate.getForObject(
+
+                    url,
+
+                    ProductDTO.class
+
+            );
+
+        }
+
+        catch(Exception e){
+
+            throw new RuntimeException(
+
+                    "Dịch vụ sản phẩm hiện không khả dụng, vui lòng thử lại sau"
+
+            );
+
+        }
+
+    }
+
 
 }
